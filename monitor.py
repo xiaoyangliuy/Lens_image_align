a524d40d564594319a740c7a31db7e35354e85f9
Lin <yuchunglin@anl.gov>
2025-03-31 16:18:06 -0500

    change ROI for 1d integration, add the monitor, circular crop option

--- monitor.py
+++ monitor.py
@@ -3,14 +3,7 @@ import os
 import sys
 import subprocess
 
-folder_to_watch = r"C:\Users\lxiaoyang\Desktop\Zyla\Zyla"  # Change this to your target directory
-# Path to the script to execute
-script_to_run = r"C:\Research\OneDrive - Argonne National Laboratory\anl\github\Data_Analysis\Alignimg_1DGaufit_fwhm_figcsv_20250302.py"
-
-# Store the existing folders
-existing_folders = set(os.listdir(folder_to_watch))
-
-def check_for_new_folders(check_time=2):
+def check_for_new_folders(check_time=2,wait_time=10):
     global existing_folders
     while True:
         current_folders = set(os.listdir(folder_to_watch))
@@ -20,10 +13,21 @@ def check_for_new_folders(check_time=2):
             folder_path = os.path.join(folder_to_watch, folder)
             if os.path.isdir(folder_path):  # Ensure it's a directory
                 print(f"New folder detected: {folder_path}")
-                run_script(folder_path)  # Pass new folder to script
-
+                prev_files = set(os.listdir(folder_path))
+                while True:
+                    print(f'wait {wait_time} to check num of files in {folder_path}')
+                    time.sleep(wait_time)  # check files every 10 seconds
+                    current_files = set(os.listdir(folder_path))
+                    if current_files != prev_files:
+                        print(f"Taking data, now {len(os.listdir(folder_path))}")
+                        prev_files = current_files
+                        prev_files = set(os.listdir(folder_path))
+                    else:
+                        print(f"Complete for {folder_path}, total {os.listdir(folder_path)}")
+                        run_script(folder_path)  # Pass new folder to script
+                        break
         existing_folders = current_folders
-        time.sleep(check_time)  # Check every 2 seconds (adjust as needed)
+        time.sleep(check_time)  # Check monitor folder every 2 seconds (adjust as needed)
         
 def run_script(folder_path):
     """Execute the script and pass the new folder path as an argument."""
@@ -33,5 +37,13 @@ def run_script(folder_path):
     except subprocess.CalledProcessError as e:
         print(f"Error executing script: {e}")
 if __name__ == "__main__":
-    print(f"Monitoring {folder_to_watch} for new folders coming in")
-    check_for_new_folders()       
+    folder_to_watch = r'/data/Beamline1BM/Mar2025/STTR/Zyla/data'  # Change this to your target directory
+    # Path to the script to execute
+    script_to_run = r'/data/Beamline1BM/Mar2025/scripts/Lens_image_align/Alignimg_1DGaufit_fwhm_figcsv_20250302.py'
+
+    # Store the existing folders
+    existing_folders = set(os.listdir(folder_to_watch))
+    print(f"Monitoring {folder_to_watch} for new folders coming in") 
+    check_time = 20 #time to check monitor folder
+    wait_time = 10 #time to check coming data files
+    check_for_new_folders(check_time=check_time,wait_time=wait_time)  
